01939dc1e2e254aaed5cf66824f52940c7077df5
Rifky Kurniawan Putra <krifky14@gmail.com>
2025-07-08 00:18:20 +0700

    feat: enhance UpdateUserDto with validation decorators for improved data integrity

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,4 +1,21 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateUserDto } from "./create-user.input";
+import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsNotEmpty, MinLength, IsEmail, IsBoolean } from 'class-validator';
+import { CreateUserDto } from './create-user.input';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsBoolean()
+  isActive?: boolean = true;
+}
