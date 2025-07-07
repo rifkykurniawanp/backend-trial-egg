9f8165641a16ff292d71733da5cd86e90c17a55c
Rifky Kurniawan Putra <krifky14@gmail.com>
2025-07-08 00:10:48 +0700

    style: format code for better readability in users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { plainToInstance } from 'class-transformer';
@@ -6,29 +13,28 @@ import { CreateUserDto } from './dto/create-user.input';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Get()
-    async getUsers(): Promise<User[]> {
-        const users = await this.usersService.listUsers();
+  @Get()
+  async getUsers(): Promise<User[]> {
+    const users = await this.usersService.listUsers();
+    return plainToInstance(User, users, { excludeExtraneousValues: true });
+  }
 
-        return plainToInstance(User, users, { excludeExtraneousValues: true });
+  @Get(':username')
+  async getUserByUsername(@Param('username') username: string): Promise<User> {
+    const userByUsername = await this.usersService.userByUsername(username);
+    if (!userByUsername) {
+      throw new Error('User not found');
     }
+    return plainToInstance(User, userByUsername, { excludeExtraneousValues: true });
+  }
 
-    @Get(':username')
-    async getUserByUsername(@Param('username') username: string): Promise<User> {
-        const userByUsername = await this.usersService.userByUsername(username);
-        if (!userByUsername) {
-            throw new Error('User not found');
-        }
-        return plainToInstance(User, this.usersService, { excludeExtraneousValues: true });
-    }
-
-    @Post()
-    async createUser(
-        @Body(new ValidationPipe()) createUserDto: CreateUserDto,
-    ) {
-        const newUser = await this.usersService.createUser(createUserDto);
-        return plainToInstance(User, newUser, { excludeExtraneousValues: true });
-    }
+  @Post()
+  async createUser(
+    @Body(new ValidationPipe()) createUserDto: CreateUserDto,
+  ) {
+    const newUser = await this.usersService.createUser(createUserDto);
+    return plainToInstance(User, newUser, { excludeExtraneousValues: true });
+  }
 }
